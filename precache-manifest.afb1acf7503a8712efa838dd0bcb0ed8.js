7265b9d5cb53429bd88d0973472b25b686c8a32f
Jc7j <chiangjason19@gmail.com>
2019-12-22 17:27:51 -0800

    Updates

---
+++ precache-manifest.afb1acf7503a8712efa838dd0bcb0ed8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24e967ec3057b15257d82ed5eea07c0",
+    "url": "/jc7j-websitev1/index.html"
+  },
+  {
+    "revision": "726f25775bb50db7452a",
+    "url": "/jc7j-websitev1/static/css/main.07855de2.chunk.css"
+  },
+  {
+    "revision": "cff68b9784b98b54b13a",
+    "url": "/jc7j-websitev1/static/js/2.3c7a191b.chunk.js"
+  },
+  {
+    "revision": "726f25775bb50db7452a",
+    "url": "/jc7j-websitev1/static/js/main.cb973119.chunk.js"
+  },
+  {
+    "revision": "c1169a1ac08b9375ab6d",
+    "url": "/jc7j-websitev1/static/js/runtime-main.02940f46.js"
+  }
+]);
