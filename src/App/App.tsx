f998c02adcf342fae87fa3fa6a8e6c1080cec38c
Jc7j <chiangjason19@gmail.com>
2019-12-22 16:33:44 -0800

    Added some new eslint rule along with some css and text additions

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -3,9 +3,16 @@ import Header from '../Components/Header/Header';
 import './App.modules.css';
 
 const App: React.FC = () => {
+  const headerDescription: string =
+    'Skilled with using React, Redux, Typescript and more to develop beautiful, interactive websites.';
+
   return (
     <div className="appContainer">
-      <Header name="Jason Chiang" subHeading="Software Engineer in Las Vegas" />
+      <Header
+        name="Jason Chiang"
+        subHeading="Software Engineer in Las Vegas"
+        description={headerDescription}
+      />
     </div>
   );
 };
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,14 +5,16 @@ import './Header.modules.css';
 interface HeaderProps {
   name: string;
   subHeading: string;
+  description: string;
 }
 
 const Header = (props: HeaderProps) => {
-  const { name, subHeading } = props;
+  const { description, name, subHeading } = props;
   return (
     <header className="headerContainer">
       <h1 className="nameText">{name}</h1>
       <h2 className="nameSubHeading">{subHeading}</h2>
+      <p className="description">{description}</p>
       <section className="linkSection">
         <Link
           link="https://www.linkedin.com/in/jasonchiangjc7j/"
@@ -20,6 +22,7 @@ const Header = (props: HeaderProps) => {
         />
         <Link link="https://github.com/Jc7j" linkText="Github" />
         <Link link="mailto:chiangjason19@gmail.com?subject=" linkText="email" />
+        <Link link="#" linkText="Resume" />
       </section>
     </header>
   );
