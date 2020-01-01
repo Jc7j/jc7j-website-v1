9d3e262346dc7c0607657fba521254c857cf372a
Jason Chiang <chiangjason67@gmail.com>
2019-12-31 17:11:07 -0800

    Added description text in Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,12 @@ import { HashRouter } from 'react-router-dom';
 import './App.modules.css';
 
 const App: React.FC = () => {
-  const headerDescription: string =
-    'Skilled with using React, Redux, Typescript and more to develop beautiful, interactive websites.';
-
   return (
     <HashRouter basename="/">
       <div className="appContainer">
         <Header
           name="Jason Chiang"
           subHeading="Software Engineer in Las Vegas"
-          description={headerDescription}
         />
       </div>
     </HashRouter>
--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -5,16 +5,26 @@ import './Header.modules.css';
 interface HeaderProps {
   name: string;
   subHeading: string;
-  description: string;
 }
 
 const Header = (props: HeaderProps) => {
-  const { description, name, subHeading } = props;
+  const { name, subHeading } = props;
+
   return (
     <header className="headerContainer">
       <h1 className="nameText">{name}</h1>
       <h2 className="nameSubHeading">{subHeading}</h2>
-      <p className="description">{description}</p>
+      <p>
+        Hi there! My name is Jason and I'm a self-taught software engineer at a
+        company based in Las Vegas, Nevada. I'm also serving the United States
+        Army National Guard as a Combat Medic.
+      </p>
+      <p>
+        In my spare time, I enjoy studying how to solve problems using different
+        data strucutures and algorithms, I also spend time with friends, my
+        golden retriever/great pyrenees mix, and staying healthy by working out
+        and playing basketball.
+      </p>
       <section className="linkSection">
         <Link
           link="https://www.linkedin.com/in/jasonchiangjc7j/"
